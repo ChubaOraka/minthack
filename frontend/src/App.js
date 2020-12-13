b8110a70f4a9bb8e93047d57755aa904c02776be
ChubaOraka <ChubaOraka@users.noreply.github.com>
2020-12-12 21:54:25 -0500

    Customizing frontend for MintHack

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import { Header } from './components/Header'
 import { Users } from './components/Users'
 import { DisplayBoard } from './components/DisplayBoard'
 import CreateUser from './components/CreateUser'
-import { getAllUsers, createUser } from './services/UserService'
+// import { getAllUsers, createUser } from './services/UserService'
 
 class App extends Component {
 
@@ -13,22 +13,6 @@ class App extends Component {
     user: {},
     users: [],
     numberOfUsers: 0
-  }
-
-  createUser = (e) => {
-      createUser(this.state.user)
-        .then(response => {
-          console.log(response);
-          this.setState({numberOfUsers: this.state.numberOfUsers + 1})
-      });
-  }
-
-  getAllUsers = () => {
-    getAllUsers()
-      .then(users => {
-        console.log(users)
-        this.setState({users: users, numberOfUsers: users.length})
-      });
   }
 
   onChangeForm = (e) => {
@@ -48,28 +32,6 @@ class App extends Component {
     return (
       <div className="App">
         <Header></Header>
-        <div className="container mrgnbtm">
-          <div className="row">
-            <div className="col-md-8">
-                <CreateUser 
-                  user={this.state.user}
-                  onChangeForm={this.onChangeForm}
-                  createUser={this.createUser}
-                  >
-                </CreateUser>
-            </div>
-            <div className="col-md-4">
-                <DisplayBoard
-                  numberOfUsers={this.state.numberOfUsers}
-                  getAllUsers={this.getAllUsers}
-                >
-                </DisplayBoard>
-            </div>
-          </div>
-        </div>
-        <div className="row mrgnbtm">
-          <Users users={this.state.users}></Users>
-        </div>
       </div>
     );
   }
