505dea22afa9465dad5e3b8c65287900aa05b7fe
ChubaOraka <ChubaOraka@users.noreply.github.com>
2020-12-14 05:39:21 -0500

    Refactoring app :twisted_rightwards_arrows: and debugging :bug: -- Converted App to a function component thereby fixing the problem of using react hooks
    Source:
    How to convert class component to functional components: https://www.digitalocean.com/community/tutorials/five-ways-to-convert-react-class-components-to-functional-components-with-react-hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,43 +8,25 @@ import useLocalStorage from './hooks/useLocalStorage'
 import CreateUser from './components/CreateUser'
 // import { getAllUsers, createUser } from './services/UserService'
 
-class App extends Component {
+function App() {
 
-  state = {
-    js: "",
-    code: useLocalStorage('code', ''),
-    users: [],
-    numberOfUsers: 0
-  }
+  const [code, setCode] = useLocalStorage('code', '')
 
-  onChangeForm = (e) => {
-    let user = this.state.user
-    if (e.target.name === 'firstname') {
-      user.firstName = e.target.value;
-    } else if (e.target.name === 'lastname') {
-      user.lastName = e.target.value;
-    } else if (e.target.name === 'email') {
-      user.email = e.target.value;
-    }
-    this.setState({ user })
-  }
 
-  render() {
-
-    return (
-      <div className="App">
-        <Header></Header>
-        <div className="main">
-          <Editor
-            language="javascript"
-            //displayName="JS"
-            value={this.state.code}
-            onChange={(input)=> this.setState({code: input}) }
-          />
-        </div>
+  return (
+    <div className="App">
+      <Header></Header>
+      <div className="main">
+        <Editor
+          language="javascript"
+          //displayName="JS"
+          value={code}
+          onChange={setCode}
+        />
       </div>
-    );
-  }
+    </div>
+  );
+
 }
 
 export default App;
