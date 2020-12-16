affab3579634766c043678decf3e9f7ee9e44f83
ChubaOraka <ChubaOraka@users.noreply.github.com>
2020-12-16 02:39:35 -0500

    Wrote a method to handle post requests. At the moment, the method takes a post from the frontend with a payload and extracts the command parameter, logs it and sends it back to the client on the front end. Ignore the cutter with the `const testDefs = ...`. Had a tough timme figuring where I can get the "command" parameter in the Resquest (req) object.
    req		https://expressjs.com/en/api.html#req
      body		https://expressjs.com/en/api.html#req.body	-- has
      the command paraeter"
      params	https://expressjs.com/en/api.html#req.params

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const path = require('path');
 const app = express(),
@@ -22,10 +23,27 @@ app.post('/backend/user', (req, res) => {
   res.json("user addedd");
 });
 
-app.get('/', (req,res) => {
+app.post('/backend/javascript/test', (req, res) => {
+  console.log('/javascript/test called!')
+
+  const testDefs = `
+  testOutput(adder(100, 23), 123);
+  testOutput(adder(23, 45), 68);
+  // add more tests here
+  `
+  
+
+  console.log(req.body.command)
+
+  res.json(req.body.command)
+  
+});
+
+
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on the port::${port}`);
+  console.log(`Server listening on the port::${port}`);
 });
