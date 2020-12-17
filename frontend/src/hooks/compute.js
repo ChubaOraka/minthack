317585929566404bd2164949f7a1034a8c103803
ChubaOraka <ChubaOraka@users.noreply.github.com>
2020-12-17 09:04:00 -0500

    Got rid of the absolute path in the compute hook that was ausing errors in deployment

--- frontend/src/hooks/compute.js
+++ frontend/src/hooks/compute.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/backend',
+    baseURL: '/backend',
 })
 
 export const commandOutput = payload => api.post(`/javascript/test`, payload)
